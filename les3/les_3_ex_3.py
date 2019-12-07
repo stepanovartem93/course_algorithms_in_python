ae4e982d09018a58ccbda518d99fe1e6ad4425e8
stepanovartem93 <stepanovartem93@gmail.com>
2019-12-07 18:03:52 +0300

    Решил задачу 1,2,3

---
+++ les3/les_3_ex_3.py
@@ -0,0 +1,19 @@
+'''3. В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.'''
+
+import random
+
+size = int(input("Введите количество элементов массива: "))
+array = [random.randint(-100, 100) for _ in range(size)]
+
+ix_min = 0
+ix_max = 0
+
+print(f'Исходный массив:\n\t{array}')
+for i in range(len(array)):
+    if array[i] < array[ix_min]:
+        ix_min = i
+    elif array[i] > array[ix_max]:
+        ix_max = i
+
+array[ix_min],array[ix_max] = array[ix_max], array[ix_min]
+print(f'Изменённый массив:\n\t{array}')
