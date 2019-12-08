6bce61cc1124c40eded02d5ba0930a78e65d94e6
stepanovartem93 <stepanovartem93@gmail.com>
2019-12-08 16:09:25 +0300

    Решил задачу 6

--- les3/les_3_ex_6.py
+++ les3/les_3_ex_6.py
@@ -1,3 +1,28 @@
 '''6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
 Сами минимальный и максимальный элементы в сумму не включать.'''
 
+import random
+
+size = int(input("Введите количество элементов массива: "))
+array = [random.randint(0, 100) for _ in range(size)]
+
+ix_min = 0
+ix_max = 0
+
+print(f'Исходный массив: {array}')
+for i in range(1, len(array)):
+    if array[i] < array[ix_min]:
+        ix_min = i
+    elif array[i] > array[ix_max]:
+        ix_max = i
+
+if ix_min > ix_max:
+    array[ix_min],array[ix_max] = array[ix_max], array[ix_min]
+
+print(f'Минимальное число: {array[ix_min]}, \nМаксимальное число: {array[ix_max]}')
+
+summa = 0
+for i in range(ix_min + 1, ix_max):
+    summa += array[i]
+
+print(f'Сумма: {summa}')
