3802b379218403fbcbef24dc6b18f222522d0614
stepanovartem93 <stepanovartem93@gmail.com>
2019-12-21 17:44:18 +0300

    выполнил сотрировку методом пузырька через функцию

--- les7/les_6_ex_1.py
+++ les7/les_6_ex_1.py
@@ -7,15 +7,17 @@ array = [i for i in range(-100, 100)]
 random.shuffle(array)
 print(f'Исходный массив:\n\t{array}')
 print('*' * 100)
-print(f'max_num = {max(array)}, mix_num = {min(array)}')
+print(f'max_num = {max(array)}, min_num = {min(array)}')
 print('*' * 100)
 
-n = 1
 
-while n < len(array):
-    for i in range(len(array) - n):
-        if array[i] > array[i + 1]:
-            array[i], array[i + 1] = array[i + 1], array[i]
-    n += 1
+def bubble_sort(array):
+    n = 1
+    while n < len(array):
+        for i in range(len(array) - n):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+        n += 1
 
-print(f'Отсортированный массив:\n\t{array}')
+bubble_sort(array)
+print(array)
