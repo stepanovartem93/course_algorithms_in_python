b4f1143278385ae4f2cb2241f1106ee56d315715
stepanovartem93 <stepanovartem93@gmail.com>
2019-12-22 17:10:45 +0300

    сортировка методом слияния

--- les7/les_7_ex_2.py
+++ les7/les_7_ex_2.py
@@ -10,16 +10,52 @@ print('*' * 100)
 print(f'max_num = {max(array)}, min_num = {min(array)}')
 print('*' * 100)
 
-def insertion_sort(array):
-    for i in range(1, len(array)):
-        spam = array[i]
-        j = i
+# def insertion_sort(array):
+#     for i in range(1, len(array)):
+#         spam = array[i]
+#         j = i
+#
+#         while array[j - 1] > spam and j > 0:
+#             array[j] = array[j - 1]
+#             j -= 1
+#
+#             array[j] = spam
+#
+# insertion_sort(array)
+# print(array)
 
-        while array[j - 1] > spam and j > 0:
-            array[j] = array[j - 1]
-            j -= 1
+def mergeSort(array):
+    # print("Splitting ",array)
+    if len(array)>1:
+        mid = len(array)//2
+        lefthalf = array[:mid]
+        righthalf = array[mid:]
 
-            array[j] = spam
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
 
-insertion_sort(array)
-print(array)
+        i=0
+        j=0
+        k=0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                array[k]=lefthalf[i]
+                i=i+1
+            else:
+                array[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i<len(lefthalf):
+            array[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j<len(righthalf):
+            array[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    # print("Merging ",array)
+
+mergeSort(array)
+print(f'Отсортированный слиянием список:\n\t{array}')
