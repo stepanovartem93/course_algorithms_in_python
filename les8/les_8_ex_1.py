6777299dc8a42c03eb78639985ab6f4e78d84e43
stepanovartem93 <stepanovartem93@gmail.com>
2019-12-25 21:10:28 +0300

    Решил задачу с количеством рукопожатий

--- les8/les_8_ex_1.py
+++ les8/les_8_ex_1.py
@@ -1,15 +1,24 @@
 '''На улице встретились N друзей. Каждый пожал руку всем остальным друзьям (по одному разу).
 Сколько рукопожатий было?'''
 
-def build_grapf(n):
-    graph = [[0] * n for element in range(n)]
+def build_graph(n):
+    graph = [[0] * n for el in range(n)]
     for i in range(len(graph)):
         for j in range(len(graph[i])):
             if i != j:
                 graph[i][j] = 1
     return graph
 
+
+def count_handsakes(graph):
+    count = 0
+    for i in range(len(graph)):
+        for j in range(len(graph[i])):
+            if graph[i][j] == 1:
+                count += 1
+    return count
+
 n = int(input("Введите количество друзей : "))
-print("Рукопожатия в виде графа :")
-print(*build_grapf(n), sep="\n")
-print(f"Количество рукопожатий равно n(n-1)/2 = {int(n * (n - 1)/2)}")
+print("Рукопожатия в виде графа: ")
+print(*build_graph(n), sep="\n")
+print(f'Количество рукопожатий: {int(count_handsakes(build_graph(n))/2)}.')
