0f80ad18b9eff33311a553850dcc9a8f6a19f1ae
sav <stepanovartem93@gmail.com>
2019-12-27 14:39:51 +0300

    добавил решрение 2 задачи 8 урока с выводом самого графа

--- les8/les_8_ex_1.py
+++ les8/les_8_ex_1.py
@@ -1,24 +1,43 @@
 '''На улице встретились N друзей. Каждый пожал руку всем остальным друзьям (по одному разу).
 Сколько рукопожатий было?'''
+'''
+Задача решена 2 способами. Первый строит таблицу смежности графа. Вторая строит сам граф.
+'''
+# def build_graph(n):
+#     graph = [[0] * n for el in range(n)]
+#     for i in range(len(graph)):
+#         for j in range(len(graph[i])):
+#             if i != j:
+#                 graph[i][j] = 1
+#     return graph
 
-def build_graph(n):
-    graph = [[0] * n for el in range(n)]
-    for i in range(len(graph)):
-        for j in range(len(graph[i])):
+
+# def count_handsakes(graph):
+#     count = 0
+#     for i in range(len(graph)):
+#         for j in range(len(graph[i])):
+#             if graph[i][j] == 1:
+#                 count += 1
+#     return count
+
+# n = int(input("Введите количество друзей : "))
+# print("Рукопожатия в виде графа: ")
+# print(*build_graph(n), sep="\n")
+# print(f'Количество рукопожатий: {int(count_handsakes(build_graph(n))/2)}.')
+
+def friend(num):
+    if num < 2:
+        return "Слишком мало друзей"
+
+    graph = []
+
+    for i in range(num):
+        for j in range(num):
             if i != j:
-                graph[i][j] = 1
-    return graph
+                graph.append((i, j))
 
+    print(graph)
 
-def count_handsakes(graph):
-    count = 0
-    for i in range(len(graph)):
-        for j in range(len(graph[i])):
-            if graph[i][j] == 1:
-                count += 1
-    return count
+    return len(graph) // 2
 
-n = int(input("Введите количество друзей : "))
-print("Рукопожатия в виде графа: ")
-print(*build_graph(n), sep="\n")
-print(f'Количество рукопожатий: {int(count_handsakes(build_graph(n))/2)}.')
+print(friend(int(input(('Сколько друзей встретилось: ')))))
